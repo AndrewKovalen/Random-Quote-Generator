ef8c3d0ef4c449bb223c3a968322e6baa35b38bb
Andrew Kovalenko <andreykoval001@gmail.com>
2021-07-27 11:30:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,62 +10,55 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-let quotes = [
-  "We are what we repeatedly do. Excellence, then, is not an act but a habit.",
-  "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
-  "Don't judge each day by the harvest you reap but by the seeds that you plant.",
-  "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-  "In the end, it's not the years in your life that count. It's the life in your years.",
-  "Many of life's failures are people who did not realize how close they were to success when they gave up.",
-  "With great power comes great responsibility."
-]
 
 /*
   Objects are made up of comma separated pairs of 
   properties and values.
 */
 
-{
-  quote: "We are what we repeatedly do. Excellence, then, is not an act but a habit."
-  source: "Will Durant"
-  citation: "The Story of Philosophy"
-  year: 1926
-}
+let quotes = [
+  {
+    quote: "We are what we repeatedly do. Excellence, then, is not an act but a habit.",
+    source: "Will Durant",
+    citation: "The Story of Philosophy",
+    year: 1926
+  },
 
-{
-  quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn."
-  source: "Benjamin Franklin"
-  citation: "Quote Encyclopedia"
-  year: 1758
-}
+  {
+    quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    source: "Benjamin Franklin",
+    citation: "Quote Encyclopedia",
+    year: 1758
+  },
 
-{
-  quote: "Don't judge each day by the harvest you reap but by the seeds that you plant."
-  source: "Robert Louis Stevenson"
-}
+  {
+    quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+    source: "Robert Louis Stevenson"
+  },
 
-{
-  quote: "The greatest glory in living lies not in never falling, but in rising every time we fall."
-  source: "Nelson Mandela"
-}
+  {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    source: "Nelson Mandela"
+  },
 
-{
-  quote: "In the end, it's not the years in your life that count. It's the life in your years."
-  source: "Abraham Lincoln"
-  year: 1865
-}
+  {
+    quote: "In the end, it's not the years in your life that count. It's the life in your years.",
+    source: "Abraham Lincoln",
+    year: 1865
+  },
 
-{
-  quote: "Many of life's failures are people who did not realize how close they were to success when they gave up."
-  source: "Thomas A. Edison"
-}
+  {
+    quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+    source: "Thomas A. Edison"
+  },
 
-{
-  quote: "With great power comes great responsibility"
-  source: "Ben Parker"
-  citation: "Movie: Spider Man"
-  year: 2002
-}
+  {
+    quote: "With great power comes great responsibility",
+    source: "Ben Parker",
+    citation: "Movie: Spider Man",
+    year: 2002
+  }
+]
 
 
 console.log(quotes);
@@ -89,13 +82,37 @@ function getRandomQuote( array ) {
 }
 
 let results = getRandomQuote(quotes);
-
 console.log(results)
+
 
 /***
  * `printQuote` function
 ***/
+function printQuote(message) {
+  // 1. Create a variable that calls the getRandomQuote() 
+  // function
+  let randomQuoteObject = getRandomQuote()
+  // 2. Create a variable that initiates your HTML string with 
+  // the first two <p></p> elements, their classNames, 
+  // and the quote and source properties, but leave off 
+  // the second closing `</p>` tag for now
+  let outcome = "<p class = 'quote'>" + randomQuote.quote + "<p class = 'source'" + randomQuote.source + "</p>"
+  // 3. Use an if statement to check if the citation property 
+  // exists, and if it does, concatenate a <span></span> 
+  // element, appropriate className, and citation property 
+  // to the HTML string
 
+  // 4. Use an if statement to check of the year property exists, 
+  // and if it does, concatenate a <span></span> element, 
+  // appropriate className, and year property to the HTML 
+  //string
+
+  // 5. After the two if statements, concatenate the closing </p> 
+  // tag to the HTML string
+
+  // 6. set the innerHTML of the quote-box div to equal the 
+  // complete HTML string
+}
 
 
 /***
@@ -103,4 +120,4 @@ console.log(results)
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
