4c2d0462c1879c83a3866291b9d4dbdad3b1b628
Andrew Kovalenko <andreykoval001@gmail.com>
2021-07-27 10:27:18 +0300

    Generating random quotes in console.

--- js/script.js
+++ js/script.js
@@ -10,14 +10,87 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  "We are what we repeatedly do. Excellence, then, is not an act but a habit.",
+  "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+  "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+  "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  "In the end, it's not the years in your life that count. It's the life in your years.",
+  "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+  "With great power comes great responsibility."
+]
+
+/*
+  Objects are made up of comma separated pairs of 
+  properties and values.
+*/
+
+{
+  quote: "We are what we repeatedly do. Excellence, then, is not an act but a habit."
+  source: "Will Durant"
+  citation: "The Story of Philosophy"
+  year: 1926
+}
+
+{
+  quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+  source: "Benjamin Franklin"
+  citation: "Quote Encyclopedia"
+  year: 1758
+}
+
+{
+  quote: "Don't judge each day by the harvest you reap but by the seeds that you plant."
+  source: "Robert Louis Stevenson"
+}
+
+{
+  quote: "The greatest glory in living lies not in never falling, but in rising every time we fall."
+  source: "Nelson Mandela"
+}
+
+{
+  quote: "In the end, it's not the years in your life that count. It's the life in your years."
+  source: "Abraham Lincoln"
+  year: 1865
+}
+
+{
+  quote: "Many of life's failures are people who did not realize how close they were to success when they gave up."
+  source: "Thomas A. Edison"
+}
+
+{
+  quote: "With great power comes great responsibility"
+  source: "Ben Parker"
+  citation: "Movie: Spider Man"
+  year: 2002
+}
 
 
+console.log(quotes);
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote( array ) {
+  // 1. Create a variable that generates a random number
+  // between zero and the last index in the `quotes` array
+  let randomNumber = Math.floor( Math.random() * quotes.length );
+  // 2. Use the random number variable and bracket notation 
+  // to grab a random object from the `quotes` array, and 
+  // store it in a variable
+  let randomQuote = array[randomNumber];
+  
 
+  // 3. Return the variable storing the random quote object
+  return randomQuote;
+}
+
+let results = getRandomQuote(quotes);
+
+console.log(results)
 
 /***
  * `printQuote` function
@@ -30,4 +103,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
